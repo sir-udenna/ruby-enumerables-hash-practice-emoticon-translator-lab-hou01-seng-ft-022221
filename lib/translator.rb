81768390673c843db99a805cbf4f7a18841ac5ab
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-12 00:14:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -33,39 +33,3 @@ def get_english_meaning(path, emoticon)
   return "Sorry, that emoticon was not found"
 end
 
-```ruby
-# require modules here
-require "yaml"
-require "pry"
-
-def load_library(file)
-  # code goes here
-  final_hash = {}
-  YAML.load_file(file).each do |key, value|
-    final_hash[key] = {}
-    final_hash[key][:english] = value[0]
-    final_hash[key][:japanese] = value[1]
-  end
-  final_hash
-end
-
-def get_japanese_emoticon(path, emoticon)
-  # code goes here
-  load_library(path).each do |key, value|
-    if value[:english] == emoticon
-      return value[:japanese]
-    end
-  end
-  return "Sorry, that emoticon was not found"
-end
-
-def get_english_meaning(path, emoticon)
-  # code goes here
-  load_library(path).each do |key, value|
-    if value[:japanese] == emoticon
-      return key
-    end
-  end
-  return "Sorry, that emoticon was not found"
-end
-```
